252e98e355e7ee8bf41695c6cd07bc43727152ee
maruki00 <aitbaha48@gmail.com>
2025-07-16 21:19:05 +0100

    commit

--- app/common.go
+++ app/common.go
@@ -78,6 +78,8 @@ var Statues = map[int]string{
 
 const CRLF = "\r\n"
 
-var NOT_FOUND = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
-
+var responses = map[string][]byte {
+	"OK" : []byte("HTTP/1.1 200 OK\r\n\r\n"),
+	"NOT_FOUND" : []byte("HTTP/1.1 404 Not Found\r\n\r\n"),
+}
 var AbsPath = "/tmp/"
