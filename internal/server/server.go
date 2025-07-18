a5dc8959d91a72e2fbe7ea9fab5b9cc0c36242ef
maruki00 <aitbaha48@gmail.com>
2025-07-18 12:32:03 +0100

    commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -24,10 +24,53 @@ func (_this *Server) Run(addr string) {
 		if err != nil {
 			panic(err)
 		}
-		go handelRequest(conn)
+		_this.NewRequest(conn)	
 	}
 }
 
+func (_this *Server) NewRequest(conn net.Conn) {
+	request, err := RequestParser(conn)
+	if err != nil {
+		conn.Write(NOT_FOUND)
+		return
+	}
+	defer conn.Close()
+	response := NewResponse(request, conn)
+	if request.EndPoint == "/" {
+		index(request, response)
+		return
+	}
+
+	if request.EndPoint == "/user-agent" {
+		userAgentReaderEndPoint(request, response)
+		return
+	}
+
+	if request.EndPoint == "/echo/abc" {
+		gzipEndPoint(request, response)
+		return
+	}
+
+	if request.isValidEndPoint("/echo/(.+)") {
+		echoEndPoint(request, response)
+		return
+	}
+
+	if request.isValidEndPoint("/files/(.+)") && request.Method == "GET" {
+		fmt.Println("request get recieved")
+		getFilesEndPoint(request, response)
+		return
+	}
+
+	if request.isValidEndPoint("/files/(.+)") && request.Method == "POST" {
+		fmt.Println("request post recieved")
+		postFilesEndPoint(request, response)
+		return
+	}
+
+	response.Write(404, []byte(Statues[404]))
+	conn.Close()
+}
 
 
 
