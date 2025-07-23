40777148610b4fb1c7a6f142ba782d589cbc903c
maruki00 <aitbaha48@gmail.com>
2025-07-23 13:02:42 +0100

    commit

--- internal/zenith/zenith_ctx.go
+++ internal/zenith/zenith_ctx.go
@@ -4,7 +4,6 @@ package zenith
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 )
 
@@ -14,33 +13,20 @@ type Context struct {
 	*Response
 }
 
-
-func (_this *Context)BindJson(obj interface{}) error {
+func (_this *Context) BindJSON(obj any) error {
 	if err := json.Unmarshal(_this.Body, &obj); err != nil {
 		return fmt.Errorf("could not parse the json : %v", err)
 	}
 	return nil
 }
 
-func (_this *Context) WriteJson(status int, obj interface{}) error {
-	data , err := json.Marshal(obj)
+func (_this *Context) WriteJSON(status int, obj any) error {
+	data, err := json.Marshal(obj)
 	if err != nil {
 		return err
 	}
-	if _,err :=  _this.Write(status, data); err != nil {
+	if err := _this.Write(status, data); err != nil {
 		return err
 	}
 	return nil
 }
-
-
-
-
-
-
-
-
-
-
-
-
