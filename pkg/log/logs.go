f037dac0465cdfaabe089438a8b5ebc9dc1e964c
maruki00 <aitbaha48@gmail.com>
2025-07-26 22:08:09 +0100

    commit

--- pkg/log/logs.go
+++ pkg/log/logs.go
@@ -5,40 +5,38 @@ import (
 	"strings"
 	"time"
 
-	"github.com/maruki00/azago/internal/common"
+	cliPkg "github.com/maruki00/azago/pkg/cli"
 )
 
-
-func Log(args ...string){
+func Log(args ...string) {
 	var logLine = strings.Builder{}
 	logLine.WriteString("[azago] ")
 	logLine.WriteString(time.Now().Format("2006/07/01 "))
-	for _,arg := range args {
+	for _, arg := range args {
 		logLine.WriteString(arg)
 		logLine.WriteString(" ")
 	}
 	println(logLine.String())
 }
 
-func Error(args ...string){
+func Error(args ...string) {
 	var logLine = strings.Builder{}
-	logLine.WriteString(fmt.Sprintf("%s[mazago] %s" , common.Red,time.Now().Format("2006/07/01 ")))
-	for _,arg := range args {
+	logLine.WriteString(fmt.Sprintf("%s[mazago] %s", cliPkg.Red, time.Now().Format("2006/07/01 ")))
+	for _, arg := range args {
 		logLine.WriteString(arg)
 		logLine.WriteString(" ")
 	}
-	logLine.WriteString(common.)
+	logLine.WriteString(cliPkg.Reset)
 	println(logLine.String())
 }
 
-func Info(args ...string){
+func Info(args ...string) {
 	var logLine = strings.Builder{}
 	logLine.WriteString("[azago] ")
 	logLine.WriteString(time.Now().Format("2006/07/01 "))
-	for _,arg := range args {
+	for _, arg := range args {
 		logLine.WriteString(arg)
 		logLine.WriteString(" ")
 	}
 	println(logLine.String())
 }
-
