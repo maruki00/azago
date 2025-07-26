16e580dd44bb387dce604eac10a7e3c03e45c72e
maruki00 <aitbaha48@gmail.com>
2025-07-26 20:04:04 +0100

    commit

--- internal/azago/server.go
+++ internal/azago/server.go
@@ -50,7 +50,7 @@ func (_this *Server) HandleRequest(conn net.Conn) {
 	response := NewResponse(request, conn)
 	route := _this.GetRoute(request.EndPoint)
 	if route.Method != request.Method{
-		response.Write(common.Statues[NOT_FOUND"], []byte("NOT FOUD"))
+		response.Write(common.Statues[NOT_FOUND"], []byte(commc))
 	} 
 	parts := strings.Split(strings.Trim(request.EndPoint, "/"), "/")
 
